82968b60b3df28f1bb01a699ea0d3627c800ad76
atzmael <atzorimaltete.mael@gmail.com>
2017-11-16 16:54:58 +0100

    Debut front

---
+++ Site/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php include_once('content/view/head.php') ?>
+</head>
+<body>
+<div class="loader" aria-hidden="true">
+    <img src="assets/img/loader.gif" alt="Image de chargement de la page" aria-hidden="true">
+</div>
+
+<div class="page">
+
+    <?php include_once('content/view/header.php') ?>
+
+    <main>
+
+    </main>
+
+    <?php include_once('content/view/footer.php') ?>
+</div>
+
+</body>
+</html>
