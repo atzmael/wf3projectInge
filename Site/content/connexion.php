73141569c5c29fe8ce0e6e057748d85731b956df
angesylvia <amonplet@gmail.com>
2017-11-24 10:57:24 +0100

    connexion
    page connexion utilisateur

--- Site/content/connexion.php
+++ Site/content/connexion.php
@@ -9,31 +9,69 @@ include_once('../content/header.php');
 
 ?>
 
+<meta charset="utf-8">
 <main class="container">
 
-<form method="POST" action="reponse_connexion.php">
+     <section class="row">
+	     <div class="col-12 carou">
 		
-		<fieldset>
+
+               	<!-- carousel slogan -->
+		<div class="col-12">
 			
-			<legend>Connexion</legend>
+			<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                  <div class="carousel-inner" role="listbox">
+                      <div class="carousel-item active">
+                          <img class="d-block img-fluid" src="..." alt=" Balance ton code">
+                     </div>
+                     <div class="carousel-item">
+                          <img class="d-block img-fluid" src="..." alt="Partage tes connaissances">
+                     </div>
+                     <div class="carousel-item">
+                        <img class="d-block img-fluid" src="..." alt="Rejoint la communauté">
+                     </div>
+                 </div>
+             </div>
+		    </div>   
 
-			<p>
-				<label for="email">Entrez votre email</label>
+
+
+	     </div>
+     </section>
+
+	<section class="row section1"> 
+
+		<div class="col-12 formulaire">
+
+
+             <form method="POST" action="reponse_connexion.php">
+
+		
+		      <fieldset>
+			
+			      <legend>>connexion_</legend>
+
+			<p><label class="label1" for="email">Entrez votre email:</label></p>
 				<input type="text" name="email" id="email">
-			</p>
+			
 
-			<p>
-				<label for="mot_de_passe">Mot de passe:</label>
+			<p><label class="label2" for="mot_de_passe">Mot de passe:</label></p>
 				<input type="password" name="mot_de_passe" id="mot_de_passe">
-			</p>
+			
 
 			<p><a href="oubli_mdp.php">Mot de passe oublié ?</a></p>
 
-			<input type="submit" name="valid">
+			<input class="bouton" type="submit" name="connexion">
 
 		</fieldset>
 
-	</form>
+	         </form>
+
+	    </div>
+	    
+	 </section>        
+
+
 
 </main>
 
